44dd82c3b3d839c077667253fab7de0d2e643ae8
Sumit-Malviiya <sumitmalviya949@gmail.com>
2024-04-23 13:11:21 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+// const parentClass = document.querySelector("#parent-class");
+// const searchBtn = document.querySelector("#Search-btn");
+// // const divItm = document.querySelector(".input-items");
+
+
+// searchBtn.addEventListener("click", function getData() {
+//     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=egg`)
+//     .then(Response => Response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+// });
+
